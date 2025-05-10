3fb0a836dd7c1a50f749856aea48b41bd6180310
Darya <dasha1234495@gmail.com>
2025-05-11 00:27:32 +0300

    add feedback, footer, typing text on landing

--- app/components/carousel/carousel.tsx
+++ app/components/carousel/carousel.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useEffect, useState } from 'react';
 import ReactMultiCarousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
@@ -33,22 +33,31 @@ const responsive = {
   },
 };
 
-export const Carousel = ({ children }: ICarouselProps) => (
-  <ReactMultiCarousel
-    responsive={responsive}
-    additionalTransfrom={0}
-    arrows
-    autoPlaySpeed={3000}
-    draggable
-    infinite
-    keyBoardControl
-    minimumTouchDrag={80}
-    pauseOnHover
-    shouldResetAutoplay
-    slidesToSlide={1}
-    swipeable
-    // itemClass='carousel-item-padding-40-px'
-  >
-    {children}
-  </ReactMultiCarousel>
-);
+export const Carousel = ({ children }: ICarouselProps) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
+  return (
+    <ReactMultiCarousel
+      responsive={ responsive }
+      additionalTransfrom={ 0 }
+      arrows
+      autoPlaySpeed={ 3000 }
+      draggable
+      infinite
+      keyBoardControl
+      minimumTouchDrag={ 80 }
+      pauseOnHover
+      shouldResetAutoplay
+      slidesToSlide={ 1 }
+      swipeable
+    >
+      { children }
+    </ReactMultiCarousel>
+  );
+};
